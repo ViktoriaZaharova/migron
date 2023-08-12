2ba76fdf1fe99c0a2c37fc46fd9671780b31dc44
Виктория Захарова <zaharova181114@gmail.com>
2023-08-12 23:20:39 +0300

    add section

--- js/common.js
+++ js/common.js
@@ -2,3 +2,18 @@
 $('.btn-burger').on('click', function () {
 	$('.mobile-menu').toggleClass('open');
 });
+
+$('.sales-slider').slick({
+	slidesToShow: 1,
+	fade: true,
+	nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>',
+	prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>'
+});
+
+$('.reviews-slider').slick({
+	slidesToShow: 3,
+	variableWidth: true,
+	appendArrows: '.reviews-slider__nav',
+	nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>',
+	prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>'
+});
