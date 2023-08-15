aa68a7e88c5a2f2f5d1270c0eb516fbfe6643563
Виктория Захарова <zaharova181114@gmail.com>
2023-08-15 09:40:35 +0300

    add media1

--- js/common.js
+++ js/common.js
@@ -24,8 +24,28 @@ $('.address-gallery').slick({
 	prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>'
 });
 
+$('.form-survey-slider').slick({
+	slidesToShow: 1,
+	fade: true,
+	infinite: false,
+	appendArrows: '.form-survey-slider__nav',
+	nextArrow: '<button type="button" class="slick-next btn btn-md btn-accent"><span>следующий вопрос</span><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>',
+	prevArrow: '<button type="button" class="slick-prev btn btn-md btn-border-accent"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg><span>назад</span></button>'
+});
+
+$(".form-survey-slider").on("afterChange", function (event) {
+	if ($(this).find('.slick-slide').last().hasClass('slick-active')) {
+		$('.form-survey').addClass('last-slide');
+	} 
+});
+
+$(".form-survey-slider").on("beforeChange", function (event) {
+	if ($(this).find('.slick-slide').last().hasClass('slick-active')) {
+		$('.form-survey').removeClass('last-slide');
+	} 
+});
 
 $('.modal').on('shown.bs.modal', function () {
-	$('.address-gallery').resize();
-	$('.address-gallery').slick('setPosition');
+	$('.slick-slider').resize();
+	$('.slick-slider').slick('setPosition');
 });
